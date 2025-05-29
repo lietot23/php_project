e34d23302248e452ba5d1145ed512857d3aa15f2
unknown <gvillasencio2001@gmail.com>
2025-05-30 01:17:05 +0800

    food

---
+++ components/footer.php
@@ -0,0 +1,35 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>Our email</h3>
+         <a href="mailto:shaikhanas@gmail.com">inspire3@gmail.com</a>
+         
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>Opening Hours</h3>
+         <p>07:00am to 10:00pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>Our address</h3>
+         <a href="#">Cebu, Philippines</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>Our number</h3>
+         
+         <a href="tel:1112223333">0909-852-4567</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Group1-Inspire</span> | all rights reserved!</div>
+
+</footer>
